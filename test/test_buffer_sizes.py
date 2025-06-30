3ba467c7a600daa7f3d30d9744ed1ec1d2b8fb3a
Tamas Nepusz <ntamas@gmail.com>
2025-07-01 01:23:53 +0200

    style: run formatter

--- test/test_buffer_sizes.py
+++ test/test_buffer_sizes.py
@@ -11,9 +11,9 @@ async def test_maximize_socket_receive_buffer_size():
     sock = create_socket(trio.socket.SOCK_STREAM)
     original_size = sock.getsockopt(trio.socket.SOL_SOCKET, trio.socket.SO_RCVBUF)
     new_size = maximize_socket_receive_buffer_size(sock)
-    assert new_size > original_size, (
-        "The new receive buffer size should be larger than the original size."
-    )
+    assert (
+        new_size > original_size
+    ), "The new receive buffer size should be larger than the original size."
 
     even_newer_size = maximize_socket_receive_buffer_size(sock)
     assert new_size == even_newer_size, (
@@ -27,9 +27,9 @@ async def test_maximize_socket_send_buffer_size():
     original_size = sock.getsockopt(trio.socket.SOL_SOCKET, trio.socket.SO_SNDBUF)
 
     new_size = maximize_socket_send_buffer_size(sock)
-    assert new_size > original_size, (
-        "The new send buffer size should be larger than the original size."
-    )
+    assert (
+        new_size > original_size
+    ), "The new send buffer size should be larger than the original size."
 
     even_newer_size = maximize_socket_send_buffer_size(sock)
     assert new_size == even_newer_size, (
